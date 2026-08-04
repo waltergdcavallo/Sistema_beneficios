c2e511bc31c3ceba54fa0dceed53cf000658b734
Walter <wosinalde@gmail.com>
2026-08-04 11:47:36 -0300

    sass

--- vistas/sesion/login.php
+++ vistas/sesion/login.php
@@ -20,7 +20,7 @@
 </head>
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="http://localhost/sistema_beneficios/vistas/inicio.php">Sistema de beneficios</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
